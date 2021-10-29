0a44c50ba0093a609d34cb845bf18b1df9db5b5a
MA_yo_TA <m.yota0906kapa@gmail.com>
2021-10-30 03:34:57 +0900

    テストを修正

--- test.sh
+++ test.sh
@@ -18,8 +18,6 @@ assert() {
 
 assert 0 0
 assert 42 42
-assert 1 "+1"
-assert 10 "5+ 5"
 assert 21 "5+20-4"
 assert 41 " 12 + 34 - 5 "
 assert 47 "5+6*7"
@@ -30,6 +28,6 @@ assert 10 "-10 + 20"
 assert 1 "10 < 2 * 10"
 assert 0 "10 >= (+2 * +10)"
 assert 0 "(10 < 2 * 10) == (10 >= (+2 * +10))"
-assert 1 "(10 < 2 * 10) != (10 >= (+2 * +10))"
+assert 1 "(20 > 10) != (10 >= (+2 * +10))"
 
 echo OK
